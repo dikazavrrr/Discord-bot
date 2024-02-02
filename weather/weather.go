e21f5396ac58789ac4054673aea31b2b8ccadd7f
Diana Tastanbek <dikatastanbek@gmail.com>
2024-02-02 19:00:20 +0600

    added comment

--- weather/weather.go
+++ weather/weather.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// Represent the structure of the JSON response from the weather API
 type weather struct {
 	Location struct {
 		Name string `json:"name"`
@@ -19,6 +20,7 @@ type weather struct {
 	} `json:"current"`
 }
 
+// Weather fetches weather information for the specified location using the weather API
 func Weather(location string) (string, float64, string) {
 	key := "35bd76d57c7f4652add204524240102"
 
@@ -33,18 +35,21 @@ func Weather(location string) (string, float64, string) {
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 
+	//Perform the API request
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Print(err.Error())
 	}
 
+	//Read the response body
 	defer resp.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Print(err.Error())
 	}
+
+	//Unmarshal the JSON response into the weather struct
 	var responseObject weather
 	json.Unmarshal(bodyBytes, &responseObject)
 	return responseObject.Location.Name, responseObject.Current.TempC, responseObject.Current.Condition.Text
-
 }
